f7d5257747b2d9dc833641f1bbb961d07ddf6125
keianwinsuzuki <yuta.suzuki.q6@dc.tohoku.ac.jp>
2024-11-19 20:01:32 +0900

    add: html/css/js

---
+++ script/script.js
@@ -0,0 +1,34 @@
+let textareaIn = "";
+let textareaOut = "";
+
+// window.onload = function() {
+//     processText();
+// };
+
+function processText() {
+    let textIn = document.getElementById("inputText").value;
+    //
+    // console.log("processText: " + textIn);
+    let textOut = "";
+    let text_temp = "";
+    let is_live = false;
+    //
+    if (textIn.includes("/live")) {
+        is_live = true;
+    }
+    console.log("is_live: " + is_live);
+    //
+    if (textIn.startsWith("https://")) {
+        text_temp = textIn.substring(8);
+        if (text_temp.startsWith("www.youtube.com") && text_temp.includes("watch?v=")) {
+            text_temp = text_temp.split("watch?v=")[1];
+        }else if (text_temp.startsWith("youtu.be") && text_temp.includes("youtu.be/")) {
+            text_temp = text_temp.split("youtu.be/")[1];
+        }
+        console.log("text_temp: " + text_temp);
+        textOut = "https://m.youtube.com/watch?v=" + text_temp;
+    }else{
+        text_temp = "Invalid URL";
+    }
+    document.getElementById("outputText").value = textOut;
+}
